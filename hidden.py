4e76f1551c84530703180b38bfe3490c884aa119
romanyshyn-natalia <romanyshyn.n@ucu.edu.ua>
2020-02-24 22:00:54 +0200

    Initial commit

--- hidden.py
+++ hidden.py
@@ -2,7 +2,7 @@
 # https://apps.twitter.com/
 
 def oauth():
-    return {"consumer_key": "gxgx1lzCRBJKZbMxtWAg7r0F7",
-            "consumer_secret": "L9ndySwsq4VH4tgQEQIyi8HJy4tdZokAa7hPexs89setr2tPXY",
-            "token_key": "1075452249634148352-EBRIoLfqyLaDKrNbvddmatBbEIjumh",
-            "token_secret": "3jCcle42zs3V5wAoTTaPSoISjvf1e7TBgJblzRSTbtRy0"}
+    return {"consumer_key": 'Type your key here',
+            "consumer_secret": 'Type your key here',
+            "token_key": 'Type your key here',
+            "token_secret": 'Type your key here'}
