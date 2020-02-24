ae127a8b529e47d6c0d1e13519c5d53c45a37f72
romanyshyn-natalia <romanyshyn.n@ucu.edu.ua>
2020-02-24 16:30:01 +0200

    add changes

---
+++ flask_app.py
@@ -0,0 +1,20 @@
+from flask import Flask, redirect, url_for, request, render_template, abort
+import create_map
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/get_name', methods=["POST"])
+def get_name():
+    name = request.form.get("name")
+    create_map.main(name)
+    return render_template("/" + name + "_map.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
